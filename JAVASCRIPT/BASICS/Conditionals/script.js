bd5cd5bee3311058ab2787096cecd8448edcad03
paulbarugh <work@paulbarugh.com>
2018-04-05 16:53:52 +0100

    js practice

--- JAVASCRIPT/BASICS/Conditionals/script.js
+++ JAVASCRIPT/BASICS/Conditionals/script.js
@@ -6,38 +6,38 @@ var otherwise = "You can enter and have a drink";
 var age = Number(prompt("What is your age?"));
 
 if (age === undefined) {
-    alert(Error);
+  alert(Error);
 
 }
 if (age < 18) {
-    alert(under18);
+  alert(under18);
 } else if (age < 21) {
-    alert(between1821);
+  alert(between1821);
 } else if (age === 21) {
-    console.log("Happy Birthday!");
+  console.log("Happy Birthday!");
 } else {
-    alert(otherwise);
+  alert(otherwise);
 }
 
 
 // If age is negative
-if(age < 0) {
- alert("Come back once you're out of the womb");
+if (age < 0) {
+  alert("Come back once you're out of the womb");
 }
- 
-// If age is 21  
-if(age === 21) {
- alert("Happy 21st Birthday!");
+
+// If age is 21
+if (age === 21) {
+  alert("Happy 21st Birthday!");
 }
- 
+
 // If age is odd
 // Age divided by two - if it doesn't have a remainder of zero
 //(not evenly divisible by two)
-if(age % 2 !== 0) {
- alert("Your age is odd!");
+if (age % 2 !== 0) {
+  alert("Your age is odd!");
 }
- 
+
 // If age is a perfect square
-if(age % Math.sqrt(age) === 0) {
+if (age % Math.sqrt(age) === 0) {
   alert("Your age is a perfect square!");
 }
--- JAVASCRIPT/BASICS/FUNCTIONS/arguments/problemSet.js
+++ JAVASCRIPT/BASICS/FUNCTIONS/arguments/problemSet.js
@@ -16,7 +16,22 @@ function factorial(num) {
   if (num === 0)
     return 0;
   for (var i = num - 1; i >= 1; i--) {
-    num *= i;
+    num *= i; // num = num x i (left side must be a variable)
   }
   return num;
+}
+
+
+//Function that replaces certain character with other characters
+
+function kebabToSnake(str) {
+    for (var i = 0; i < str.length; i++)
+}
+
+
+var count = 0;
+
+while (count < str.length) {
+    console.log(str[count]);
+    count++;
 }
