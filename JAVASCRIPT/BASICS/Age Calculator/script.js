7f47888644c4c7d04a4a37aa42d648e5e1f0ea72
paulbarugh <work@paulbarugh.com>
2018-04-08 22:19:07 +0100

    delete

--- JAVASCRIPT/BASICS/Age Calculator/script.js
+++
@@ -1,6 +0,0 @@
-var age = prompt ("What is your age?");
-
-var total = age * 365.25;
-
-alert (age + " years is roughly " + total + " days");
-
--- JAVASCRIPT/BASICS/Stalker demo/script.js
+++
@@ -1,9 +0,0 @@
-var firstName = prompt("What is your first name?");
-var secondName = prompt("What is your second name?");
-var age = prompt("What is your age?");
-
-var fullName = firstName + " " + secondName;
-
-console.log("Your full name is, " + fullName + ".");
-console.log("you are " + age + " years old");
-
