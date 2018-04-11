dd4ccca34160371e218a2815880a671361f9e1eb
paulbarugh <work@paulbarugh.com>
2018-04-11 18:15:24 +0100

    training

---
+++ WEBSITES/Nolymitweb/content.js
@@ -0,0 +1,24 @@
+var section1 = [{
+
+    "mainTitle": "Your AI Multi-lingual Voice chatbot with your own information provides useful answers fast in any language to Your Customers",
+
+    "block1": {
+        "title": "Your DIVERSE customers will enjoy Great Experience to obtain answers from your chatbot fast in their native languages 24/7",
+        "content": "Obtaining answers from your chatbot with your information in above way is more ATTRACTIVE to your diverse buyers or users",
+        "cta": "Learn More"
+    },
+
+    "block2": {
+        "title": "Nolymt has built the most EFFECTIVE AI Multi-lingual Voice chatbots",
+        "content": "Your AI Voice chatbot built by Nolymit technology provides helpful answers fast in world major languages. Nolymit technology is the world leader in this field!",
+        "cta": "Learn More"
+    },
+
+    "block3": {
+        "title": "Your chatbots will quickly bring Chinese speaking buyers",
+        "content": "Nolymit uses our rich resources to market our clients' Multi-lingual chatbots to Chinese -speaking buyers in the world; You get these customers for free.",
+        "cta": "Learn More"
+    }
+}];
+
+
